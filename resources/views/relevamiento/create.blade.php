cda420c2d6e490eec97eb19eed4a1eee06ff39ce
exequieldev93 <exequiel191993@gmail.com>
2022-06-29 12:47:58 -0300

    Modal detalle

--- resources/views/relevamiento/create.blade.php
+++ resources/views/relevamiento/create.blade.php
@@ -113,7 +113,28 @@
                 </div>
                 
             </div>
-          
+            
+            {{-- Modal --}}
+            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
           <div class="form-group">
               <button class="btn btn-primary" type="submit">Guardar</button>
               <button class="btn btn-danger" type="reset">Cancelar</button>
@@ -207,7 +228,7 @@ function agregar(inc){
                 fila = fila + '<td><input type="hidden" name="apellidos[]" value="' + apellidos + '">' + apellidos + '</td>' ;
                 fila = fila + '<td><input type="hidden" name="fechaNac[]" value="' + fechaNac + '">' + fechaNac + '</td>' ;
                 fila = fila + '<td><input type="hidden" name="dni[]" value="' + dni + '">' + dni + '</td>' ;
-                fila = fila + '<td>' + '<button class="form-control">'+ 'Detalle' + '</button>' + '</td>' ;
+                fila = fila + '<td>' + '<button class="form-control" data-toggle="modal" data-target="#staticBackdrop">'+ 'Detalle' + '</button>' + '</td>' ;
                 fila = fila + '</tr>' ;
                 limpiar(inc);
 
@@ -217,6 +238,7 @@ function agregar(inc){
     alert("Error al ingresar los datos de la persona en el grupo familiar.") ;
 }
 }
+
 
 function limpiar(inc){
     $("#vinculo" + inc).val("");
